c2751393010e6ff36ee557f25ed4a3bed94ee9d7
MohamedAmgad2002 <mohamedamgad233@gmail.com>
2023-04-10 01:16:20 +0200

    Added modal menu JS code

--- scripts/viewAll.js
+++ scripts/viewAll.js
@@ -1,11 +1,26 @@
-// function collapse(id, arrowId) {
-//   var content = document.getElementById(id);
-//   var arrow = document.getElementById(arrowId);
-//   if (content.style.display == 'none') {
-//     content.style.display = 'table-row';
-//     arrow.style.transform = 'rotate(90deg)';
-//   } else {
-//     content.style.display = 'none';
-//     arrow.style.transform = 'rotate(0deg)';
-//   }
-// }
+function popUp(modalId){
+    const targetMenu = document.getElementById(modalId);
+    const targetOverlay = document.getElementsByClassName('overlay');
+    targetMenu.classList.add('active');
+    targetOverlay[0].classList.add('active');
+}
+
+function closePopUp(modalId){
+    const targetMenu = document.getElementById(modalId);
+    const targetOverlay = document.getElementsByClassName('overlay');
+    targetMenu.classList.remove('active');
+    targetOverlay[0].classList.remove('active');
+}
+
+
+function handleClick(event) {
+    const targetMenu = document.querySelectorAll('[id^="modal"]');
+    const targetOverlay = document.getElementsByClassName('overlay');
+    if (event.target !== targetMenu && !targetMenu.contains(event.target)) {
+        targetMenu.classList.remove('active');
+        targetOverlay[0].classList.remove('active');
+    }
+}
+  
+document.addEventListener('click', handleClick);
+  
