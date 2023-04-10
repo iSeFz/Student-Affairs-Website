b1175523cfb1f49cfe23d51fce8f3b678fe34c46
Yousef AlZayat <67283820+YousefAlzayat@users.noreply.github.com>
2023-04-10 02:31:17 +0200

    Lots of updates

--- scripts/viewAll.js
+++ scripts/viewAll.js
@@ -1,3 +1,57 @@
+// function popUp(modalId){
+//     const targetMenu = document.getElementById(modalId);
+//     const targetOverlay = document.getElementsByClassName('overlay');
+//     targetMenu.classList.add('active');
+//     targetOverlay[0].classList.add('active');
+// }
+
+// function closePopUp(modalId){
+//     const targetMenu = document.getElementById(modalId);
+//     const targetOverlay = document.getElementsByClassName('overlay');
+//     targetMenu.classList.remove('active');
+//     targetOverlay[0].classList.remove('active');
+// }
+
+// function handleClick(event) {
+//     console.log("TESATEST");
+//     const targetMenu = document.querySelectorAll('[id^="modal"]');
+//     const targetOverlay = document.getElementsByClassName('overlay');
+//     targetMenu.forEach((menu) => {
+//       if (event.target.id !== menu && !menu.contains(event.target)) {
+//         if (menu.classList.contains('active')) {
+//           menu.classList.remove('active');
+//           targetOverlay[0].classList.remove('active');
+//         } else {
+//           menu.classList.add('active');
+//           targetOverlay[0].classList.add('active');
+//         }
+//       }
+//     });
+//   }
+
+// // function handleClick(event) {
+// //     const targetMenu = document.querySelectorAll('[id^="modal"]');
+// //     const targetOverlay = document.getElementsByClassName('overlay');
+// //     for(var i in targetMenu){
+// //         if(event.target.id !== targetMenu[i] && !targetMenu[i].contains(event.target)){
+// //             if(!targetMenu[i].classList.contains('active')) {
+// //                 targetMenu[i].classList.add('active');
+// //                 targetOverlay[0].classList.add('active');
+// //             }else{
+// //                 targetMenu[i].remove('active');
+// //                 targetOverlay[0].classList.remove('active');
+// //             }
+// //         }
+// //     }
+// //     // if (event.target.id !== targetMenu && !targetMenu.contains(event.target)) {
+// //     //     targetMenu.classList.remove('active');
+// //     //     targetOverlay[0].classList.remove('active');
+// //     // }
+// // }
+  
+// document.addEventListener('click', handleClick);
+  
+
 function popUp(modalId){
     const targetMenu = document.getElementById(modalId);
     const targetOverlay = document.getElementsByClassName('overlay');
@@ -12,15 +66,23 @@ function closePopUp(modalId){
     targetOverlay[0].classList.remove('active');
 }
 
-
 function handleClick(event) {
-    const targetMenu = document.querySelectorAll('[id^="modal"]');
+    const targetMenus = document.querySelectorAll('[id^="modal"]');
     const targetOverlay = document.getElementsByClassName('overlay');
-    if (event.target !== targetMenu && !targetMenu.contains(event.target)) {
-        targetMenu.classList.remove('active');
-        targetOverlay[0].classList.remove('active');
+    targetMenus.forEach((menu) => {
+        if (event.target.id === menu.dataset.trigger) {
+            popUp(menu.id);
+        } else if (event.target.classList.contains('close')) {
+            closePopUp(event.target.closest('.modal').id);
+        } else if (event.target.classList.contains('overlay')) {
+            closePopUp(document.querySelectorAll('[id^="modal"].active')[0].id);
+        }
+    });
+    if (document.querySelectorAll('[id^="modal"].active').length > 0) {
+      targetOverlay[0].classList.add('active');
+    } else {
+      targetOverlay[0].classList.remove('active');
     }
 }
-  
+
 document.addEventListener('click', handleClick);
-  
