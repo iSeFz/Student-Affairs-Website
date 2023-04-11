9f3feaace66e15af9a27fba289a6285d50431ed4
Mohamed Amgad <mohamedamgad233@gmail.com>
2023-04-11 05:17:03 +0200

    Made viewAll page more interactive

--- scripts/viewAll.js
+++ scripts/viewAll.js
@@ -1,57 +1,3 @@
-// function popUp(modalId){
-//     const targetMenu = document.getElementById(modalId);
-//     const targetOverlay = document.getElementsByClassName('overlay');
-//     targetMenu.classList.add('active');
-//     targetOverlay[0].classList.add('active');
-// }
-
-// function closePopUp(modalId){
-//     const targetMenu = document.getElementById(modalId);
-//     const targetOverlay = document.getElementsByClassName('overlay');
-//     targetMenu.classList.remove('active');
-//     targetOverlay[0].classList.remove('active');
-// }
-
-// function handleClick(event) {
-//     console.log("TESATEST");
-//     const targetMenu = document.querySelectorAll('[id^="modal"]');
-//     const targetOverlay = document.getElementsByClassName('overlay');
-//     targetMenu.forEach((menu) => {
-//       if (event.target.id !== menu && !menu.contains(event.target)) {
-//         if (menu.classList.contains('active')) {
-//           menu.classList.remove('active');
-//           targetOverlay[0].classList.remove('active');
-//         } else {
-//           menu.classList.add('active');
-//           targetOverlay[0].classList.add('active');
-//         }
-//       }
-//     });
-//   }
-
-// // function handleClick(event) {
-// //     const targetMenu = document.querySelectorAll('[id^="modal"]');
-// //     const targetOverlay = document.getElementsByClassName('overlay');
-// //     for(var i in targetMenu){
-// //         if(event.target.id !== targetMenu[i] && !targetMenu[i].contains(event.target)){
-// //             if(!targetMenu[i].classList.contains('active')) {
-// //                 targetMenu[i].classList.add('active');
-// //                 targetOverlay[0].classList.add('active');
-// //             }else{
-// //                 targetMenu[i].remove('active');
-// //                 targetOverlay[0].classList.remove('active');
-// //             }
-// //         }
-// //     }
-// //     // if (event.target.id !== targetMenu && !targetMenu.contains(event.target)) {
-// //     //     targetMenu.classList.remove('active');
-// //     //     targetOverlay[0].classList.remove('active');
-// //     // }
-// // }
-  
-// document.addEventListener('click', handleClick);
-  
-
 function popUp(modalId){
     const targetMenu = document.getElementById(modalId);
     const targetOverlay = document.getElementsByClassName('overlay');
@@ -86,3 +32,92 @@ function handleClick(event) {
 }
 
 document.addEventListener('click', handleClick);
+
+
+let students = JSON.parse(localStorage.getItem('students'));
+
+function createModal(student,table,idx){
+    let modal = document.createElement('div');
+    modalHead = document.createElement('div');
+    title = document.createElement('div');
+    existButton = document.createElement('button');
+    modalBody = document.createElement('div');
+    // customize modal menu
+    modal.classList.add('modal');
+    modal.id = 'modal' + idx;
+    // customize modal head
+    modalHead.classList.add('modal-head');
+    title.classList.add('title');
+    existButton.classList.add('close-button');
+    existButton.textContent = '\u00D7';
+    let modalId = 'modal' + idx;
+    existButton.setAttribute('onclick',`popUp('${modalId}')`)
+    //customize modal body
+    modalBody.classList.add('modal-body')
+
+
+    modalHead.appendChild(title);
+    modalHead.appendChild(existButton);
+    modal.appendChild(modalHead);
+    modal.appendChild(modalBody);
+    table.appendChild(modal);
+
+}
+
+
+function createRow(student,idx){
+    let tbody = document.querySelector('tbody');
+    let newRow = document.createElement('tr');
+
+    // table cells
+    let cell1 = document.createElement('td');
+    cell1.textContent = student.name;   
+    let cell2 = document.createElement('td');
+    cell2.textContent = student.id;
+    let cell3 = document.createElement('td');
+    cell3.textContent = student.gpa;
+    let cell4 = document.createElement('td');
+    cell4.textContent = student.level;
+    let cell5 = document.createElement('td');
+    let cell6 = document.createElement('td');
+    
+    // creat modal menu button
+    let modalId = 'modal' + idx;
+    cell6.setAttribute('onclick',`popUp('${modalId}')`)
+
+    // create checkbox div
+    let checkbox = document.createElement('div');
+    checkbox.classList.add('checkbox');
+    // create input element
+    let input = document.createElement('input');
+    input.type = 'checkbox';
+    input.value = 'Active';
+    input.id = 'check' + idx;
+
+    // check status
+    if(student.status == 'active')
+        input.checked = true;
+
+    // create label
+    let label = document.createElement('label');
+    label.htmlFor = 'check' + idx;
+
+    // insert each element at it's proper position
+    checkbox.appendChild(input);
+    checkbox.appendChild(label);
+    cell5.appendChild(checkbox);
+
+    // append children to newRow
+    newRow.appendChild(cell1);
+    newRow.appendChild(cell2);
+    newRow.appendChild(cell3);
+    newRow.appendChild(cell4);
+    newRow.appendChild(cell5);
+    newRow.appendChild(cell6);
+    tbody.appendChild(newRow);
+    createModal(student,tbody,idx);
+}
+
+for(var st of students){
+    createRow(st,students.indexOf(st));
+}
