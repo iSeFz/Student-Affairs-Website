e745f4b8815a1d12979e90c861600bf22ec9ddbb
iSeFz <seifyahia@gmail.com>
2023-04-11 02:48:06 +0200

    Added local storage to store new students Created new separate JS file for Add New Student page Modified buttons to match with Edit Student page

---
+++ scripts/newStudent.js
@@ -0,0 +1,55 @@
+// Array of student objects
+let students = [];
+// Student object attributes
+let student = {
+    name: "",
+    dob: "",
+    phone: "",
+    email: "",
+    gender: "",
+    status: "",
+    id: "",
+    level: "",
+    gpa: "",
+    dept: ""
+};
+
+// Check if students array exists in local storage
+if (localStorage.getItem("students")) {
+    // Get students array from local storage
+    students = JSON.parse(localStorage.getItem("students"));
+}
+
+// Create student when the form is submitted
+function createStudent() {
+    // Get form values
+    let name = document.getElementById("name").value;
+    let dob = document.getElementById("dob").value;
+    let phone = document.getElementById("phone").value;
+    let email = document.getElementById("email").value;
+    let gender = document.querySelector("input[name=gender]:checked").value;
+    let status = document.querySelector("input[name=status]:checked").value;
+    let id = document.getElementById("id").value;
+    let level = document.getElementById("level").value;
+    let gpa = document.getElementById("gpa").value;
+    let dept = document.getElementById("dept").value;
+    // Create student object
+    let student = {
+        name: name,
+        dob: dob,
+        phone: phone,
+        email: email,
+        gender: gender,
+        status: status,
+        id: id,
+        level: level,
+        gpa: gpa,
+        dept: dept
+    };
+    // Add student object to students array
+    students.push(student);
+    // Save students array to local storage
+    localStorage.setItem("students", JSON.stringify(students));
+    // Display success message
+    alert('Student created successfully!');
+}
