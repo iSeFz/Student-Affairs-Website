45aa8f3a0d55c963ffd3280ae1910abc319fc36c
Mohamed Amgad <mohamedamgad233@gmail.com>
2023-05-18 13:27:50 +0300

    Change navigation urls to dtl syntax

--- Phase#3/StudentAffairs/WebApp/apps.py
+++ Phase#3/StudentAffairs/WebApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class WebsiteConfig(AppConfig):
+class WebappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Website'
+    name = 'WebApp'
