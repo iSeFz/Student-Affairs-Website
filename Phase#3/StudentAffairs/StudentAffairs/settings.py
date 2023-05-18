eef142948c1a2ed928079a39a6b48c7e6b727708
Mohamed Amgad <mohamedamgad233@gmail.com>
2023-05-18 13:00:03 +0300

    Linked app with the whole project

--- Phase#3/StudentAffairs/StudentAffairs/settings.py
+++ Phase#3/StudentAffairs/StudentAffairs/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'WebApp.apps.WebappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
