f0aa356771ca93f0402bf62655154b8694088b08
iSeFz <seifyahia@gmail.com>
2023-05-19 23:40:40 +0300

    Modified navbar to change relative to current page

--- Phase#3/StudentAffairs/WebApp/views.py
+++ Phase#3/StudentAffairs/WebApp/views.py
@@ -5,19 +5,19 @@ def homePage(request):
     return render(request, 'homePage.html')
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'currentPage': 'index'})
 
 def newStudent(request):
-    return render(request, 'newStudent.html')
+    return render(request, 'newStudent.html', {'currentPage': 'newStudent'})
 
 def editStudent(request):
-    return render(request, 'editStudent.html')
+    return render(request, 'editStudent.html', {'currentPage': 'editStudent'})
 
 def search(request):
-    return render(request, 'search.html')
+    return render(request, 'search.html', {'currentPage': 'search'})
 
 def viewAll(request):
-    return render(request, 'viewAll.html')
+    return render(request, 'viewAll.html', {'currentPage': 'viewAll'})
 
 def deptAssign(request):
-    return render(request, 'deptAssign.html')
+    return render(request, 'deptAssign.html', {'currentPage': 'deptAssign'})
