ff4594935a1e088fad94810590d53ccae948c495
Mohamed Amgad <mohamedamgad233@gmail.com>
2023-05-22 13:00:39 +0300

    Connected deptAssign to database

--- Phase#3/StudentAffairs/static/scripts/search.js
+++ Phase#3/StudentAffairs/static/scripts/search.js
@@ -1,55 +1,3 @@
-// global variables
-let students = JSON.parse(localStorage.getItem('students'));
-
-
-// this function is creating row with data stored in local storage
-function createRow(student, idx) {
-    let tbody = document.querySelector('tbody');
-    let newRow = document.createElement('tr');
-    newRow.classList.add('mainRow');
-    newRow.style.display = 'table-row';
-    let cells = [];
-
-    // table cells
-    cells[0] = document.createElement('td');
-    cells[0].textContent = student.name;
-    cells[1] = document.createElement('td');
-    cells[1].textContent = student.id;
-    cells[2] = document.createElement('td');
-    cells[2].textContent = student.gpa;
-    cells[3] = document.createElement('td');
-    cells[3].textContent = student.level;
-    cells[4] = document.createElement('td');
-    cells[4].innerHTML = "<a href='deptAssign.html?index=" + idx + "'>" + student.dept + "</a>";
-    cells[5] = document.createElement('td');
-    cells[5].innerHTML = "<a href='editStudent.html?index=" + idx + "'>" + "<img src='static/resources/edit.png'>" + "</a>";
-
-    // append children to newRow
-    cells.forEach((cell) => {
-        newRow.appendChild(cell);
-    });
-    tbody.appendChild(newRow);
-}
-
-// this function load info from localStorage to table
-function loadInfoToPage() {
-    if (localStorage.key(0) != null && students.length > 0) {
-        // create row for each student
-        for (var st of students) {
-            if (st.status == 'active')
-                createRow(st, students.indexOf(st));
-        }
-    }
-    else {
-        if(confirm("No Student exist to search\n Do you want to add Student")){
-            location.href = 'newStudent.html';
-        }
-        else{
-            location.href = 'index.html';
-        }
-    }
-}
-
 function searchName() {
     let searchValue = document.getElementById("studentName").value;
     let rows = document.querySelectorAll("tbody > tr");
@@ -72,8 +20,6 @@ function searchName() {
         location.href = 'search.html';
     }
 }
-
-loadInfoToPage();
 
 let searchField = document.getElementById("studentName");
 if (searchField != null) {
